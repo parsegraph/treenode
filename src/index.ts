6e9bb326abf29a16e7ebea12dfcc11ee91247676
Aaron Faanes <dafrito@gmail.com>
2022-04-26 22:37:34 -0500

    Reorder exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+import TreeNode, { ScheduleUpdateCallback } from "./TreeNode";
+import BlockTreeNode from "./BlockTreeNode";
+import TreeLabel from "./TreeLabel";
+import TreeList from "./TreeList";
 import AbstractTreeList from "./AbstractTreeList";
 import BasicTreeList from "./BasicTreeList";
-import BlockTreeNode from "./BlockTreeNode";
 import InlineTreeList from "./InlineTreeList";
 import Multislot from "./Multislot";
 import Spawner from "./Spawner";
-import TreeLabel from "./TreeLabel";
-import TreeList from "./TreeList";
-import TreeNode, { ScheduleUpdateCallback } from "./TreeNode";
 import WrappingTreeList, { NewlineTreeNode, NEWLINE } from "./WrappingTreeList";
 
 export default TreeNode;
